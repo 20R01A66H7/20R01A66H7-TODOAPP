2d8c42a579306e438fc1ab1535ee84dd9bc012e2
harshith <20r01a66h7@cmritonline.ac.in>
2023-01-17 14:21:38 +0530

    added print

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import PySimpleGUI as sg
 
 label = sg.Text("Enter a todo")
-input_text = sg.InputText(tooltip="write todo")
+input_text = sg.InputText(tooltip="write todo ❓")
 add_button = sg.Button("ADD")
 window = sg.Window('TODO', layout=[[label], [input_text, add_button]])
 window.read()
 window.close()
 
+print("hello")
